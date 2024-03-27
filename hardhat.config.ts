0dc49be8f22edb523e6e26a6c49a71ad4e161c98
kelreel <derodroll@gmail.com>
2024-03-27 20:49:57 +0300

    PushStorage: deploy scripts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,17 +2,64 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-verify";
 import "@openzeppelin/hardhat-upgrades";
+import "@nomicfoundation/hardhat-verify";
+import "hardhat-deploy";
 
 import dotenv from "dotenv";
 
 dotenv.config();
 
+const providerApiKey = process.env.ALCHEMY_API_KEY || "";
+const deployerPrivateKey =
+  process.env.PRIVATE_KEY ?? "";
+const etherscanApiKey = process.env.ETHERSCAN_API_KEY || "";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
+  defaultNetwork: "localhost",
+  namedAccounts: {
+    deployer: {
+      // By default, it will take the first Hardhat account as the deployer
+      default: 0,
+    },
+  },
   networks: {
+    optimism: {
+      url: `https://opt-mainnet.g.alchemy.com/v2/${providerApiKey}`,
+      accounts: [deployerPrivateKey],
+    },
+    optimismSepolia: {
+      url: `https://opt-sepolia.g.alchemy.com/v2/${providerApiKey}`,
+      accounts: [deployerPrivateKey],
+    },
+    base: {
+      url: `https://base-mainnet.g.alchemy.com/v2/${providerApiKey}`,
+      accounts: [deployerPrivateKey],
+    },
+    baseGoerli: {
+      url: "https://goerli.base.org",
+      accounts: [deployerPrivateKey],
+    },
+    mode: {
+      url: "https://mainnet.mode.network",
+      accounts: [deployerPrivateKey],
+    },
+    modeSepolia: {
+      url: "https://sepolia.mode.network",
+      accounts: [deployerPrivateKey],
+    },
+  },
+  etherscan: {
+    apiKey: `${etherscanApiKey}`,
+  },
+  // configuration for etherscan-verify from hardhat-deploy plugin
+  verify: {
+    etherscan: {
+      apiKey: `${etherscanApiKey}`,
+    },
   },
   sourcify: {
-    enabled: true,
+    enabled: false,
   },
 };
 
